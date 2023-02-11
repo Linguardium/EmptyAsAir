d3a7d65e24066e6f18965ce464ef7e6def47d194
Linguardium <61733861+Linguardium@users.noreply.github.com>
2023-02-11 00:29:03 -0600

    Add files via upload

---
+++ src/main/java/mod/linguardium/emptyasair/mixin/EmptyItemMixin.java
@@ -0,0 +1,23 @@
+package mod.linguardium.emptyasair.mixin;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemStack.class)
+public class EmptyItemMixin {
+    @Shadow @Final @Deprecated private Item item;
+
+    @Inject(at=@At("HEAD"),cancellable = true,method="getItem")
+    private void getItem(CallbackInfoReturnable<Item> cir) {
+        if (item == null) {
+            cir.setReturnValue(Items.AIR);
+        }
+    }
+}
